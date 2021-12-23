dbc19530077877bbee375a6972517ba83b683891
yashodhanpendse <83579944+yashodhanpendse@users.noreply.github.com>
2021-12-23 15:42:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+objects=[];
 function preload(){
 img=loadImage('dog_cat.jpg');
 }
@@ -11,6 +12,17 @@ document.getElementById("status").innerHTML="Status:Detecting Objects";
 }
 function draw(){
 image(img,0,0,640,420);
+if(status !=""){
+for(i=0;i<objects.length;i++){
+document.getElementById("status").innerHTML="Status:Object Detected";
+fill("#FF0000");
+percent=floor(objects[i].confidence*100);
+text(object[i].label+""+percent+"%",objects[i].x,objects[i].y);
+noFill();
+stroke("#FF0000");
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+}
 fill("#FF0000");
 text("dog",45,75);
 noFill();
@@ -32,4 +44,5 @@ if (error){
 console.log(error);
 }
 console.log(results);
+object=results;
 }
